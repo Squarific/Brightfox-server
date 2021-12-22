8a245e131c5307ab5782493dd319b2072b790e95
Filip Smets <filipsmetsbelgium@gmail.com>
2021-12-22 12:36:49 +0100

    Add some sensible json size limits

--- main-as-module.js
+++ main-as-module.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
     ca: fs.readFileSync(config.permfolder + '/chain.pem')
   };
 
-  app.use(express.json());
+  app.use(express.json({ limit: '10mb' }));
   app.use(cors())
 
   app.use('/plugins', require('./routes/plugins/_')(pool));
--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var pool = mysql.createPool({
   multipleStatements: true
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors())
 
 app.use('/plugins', require('./routes/plugins/_')(pool));
