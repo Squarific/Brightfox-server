e640c4c23ac7226dfb7ef69562242abf1cb880db
Filip Smets <filipsmetsbelgium@gmail.com>
2021-12-23 13:35:19 +0100

    Remove verification for versions

--- routes/versions/list.js
+++ routes/versions/list.js
@@ -1,8 +1,7 @@
 const router = require('express').Router({ mergeParams: true });
 const { param, validationResult } = require('express-validator');
 
-const SELECT_QUERY_VERIFIED = "SELECT CONCAT(major,'.',minor,'.',patch) AS version, releasenotes, creation, updatedatetime from  `versions` WHERE pluginuuid = UUID_TO_BIN(?) AND verified = true;";
-const SELECT_QUERY_UNVERIFIED = "SELECT CONCAT(major,'.',minor,'.',patch) AS version, releasenotes, creation, updatedatetime from  `versions` WHERE pluginuuid = UUID_TO_BIN(?);";
+const SELECT_QUERY = "SELECT CONCAT(major,'.',minor,'.',patch) AS version, releasenotes, creation, updatedatetime from  `versions` WHERE pluginuuid = UUID_TO_BIN(?);";
 
 const GENERIC_DB_ERROR = {
     errors: [{
@@ -12,15 +11,14 @@ const GENERIC_DB_ERROR = {
 
 module.exports = (database) => {
     router.get('/:pluginUuid/:unverified', [
-        param('pluginUuid').isLength({ min: 36, max: 36 }),
-        param('unverified')
+        param('pluginUuid').isLength({ min: 36, max: 36 })
     ], async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        database.query(req.params.unverified ? SELECT_QUERY_UNVERIFIED : SELECT_QUERY_VERIFIED, [req.params.pluginUuid], (err, result) => {
+        database.query(SELECT_QUERY, [req.params.pluginUuid], (err, result) => {
             if (err) {
                 console.log("Retrieve plugin database error", err);
                 return res.status(504).json(GENERIC_DB_ERROR);
